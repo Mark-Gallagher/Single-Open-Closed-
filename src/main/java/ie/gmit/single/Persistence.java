6b9edb8d7b44e0437c20b4ea123463f9b5512588
Mark-Gallagher <G00343007@gmit.ie>
2022-03-23 10:12:01 +0000

    First Commit Changes

---
+++ src/main/java/ie/gmit/single/Persistence.java
@@ -0,0 +1,15 @@
+package ie.gmit.single;
+
+public class Persistence extends Store {
+
+    public Persistence() {
+        //Store used by controller
+        Store store = new Store();
+        User user = null;
+        store.store(null);
+    }
+}
+
+
+
+
